9a2ccd1b70b425eec78f66b723b2cd04f4ee5e59
Margus Pala <margus.pala@gmail.com>
2016-05-04 21:25:26 +0300

    idid teenusega logimine

---
+++ id-card-login.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Plugin Name: ID-card Login
+ * Plugin URI: http://marguspala.com/
+ * Description: This plugin allows you to login to wordpress with Estonian ID-card
+ * Version: 0.1
+ * Author: Margus Pala
+ * Author URI: http://marguspala.com/
+ * License: GPLv2 or later
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+if (!class_exists("IdCardLogin")) {
+
+    class IdCardLogin {
+
+        //tekitame login nupu
+        static function show_id_login() {
+            $pUrl = plugins_url();
+            $redirect_url = strlen(array_key_exists('redirect_to', $_GET)) > 0 ? "&redirect_to=" . urlencode($_GET['redirect_to']) : "";
+            echo '<div id="idid"></div>'
+            . '<script src="https://idid.ee/js/button.js"></script>'
+            . '<script>'
+            . "new Button({ img: 5, width: 240, clientId: '022f8d04772c174a926572a125871156bb5ec12e361268407dd63530ce2523e5' }, function(token) { "
+            . "console.log(token); "
+            . 'window.location="' . $pUrl . '/id-card-login/securelogin.php?token="+token+"' . $redirect_url . '"'
+            . "});</script>";
+        }
+
+        //konfime andmebaasi
+        static function idcard_install() {
+            global $wpdb;
+
+            $table_name = $wpdb->prefix . "idcard_users";
+
+            $sql = "CREATE TABLE $table_name (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,                
+                firstname tinytext NOT NULL,
+                lastname tinytext NOT NULL,
+                identitycode VARCHAR(11) NOT NULL,
+                userid bigint(20) unsigned NOT NULL,
+                created_at datetime NOT NULL,
+                UNIQUE KEY id (id),
+                UNIQUE KEY identitycode (identitycode)
+                  );";
+
+            require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+            dbDelta($sql);
+        }
+
+    }
+
+    //registreerime wordpressiga integratsioonipunktid
+    add_action('login_form', 'IdCardLogin::show_id_login');
+    register_activation_hook(__FILE__, 'IdCardLogin::idcard_install');
+}
---
+++ securelogin.php
@@ -0,0 +1,93 @@
+k<?php
+require_once('../../../wp-load.php');
+
+IdcardAuthenticate::login();
+
+class IdcardAuthenticate {
+
+    static function login() {
+
+        $token = $_GET['token'];
+        
+        //tõmbame sisselogitud inimese andmed
+        $result = json_decode(IdcardAuthenticate::getUserFromIdid($token));
+        $firstName = $result->firstname;
+        $lastName = $result->lastname;
+        $identityCode = $result->id;
+        $userName = "EE" . $identityCode;
+
+        //Otsime üles sisselogitud inimese või tekitame, kui teda vare polnud
+        $user = IdcardAuthenticate::getUser($identityCode);
+        if (($user == NULL) and ( NULL == username_exists($userName))) {
+            $user_id = IdcardAuthenticate::createUser($userName, $firstName, $lastName, $identityCode);
+        } else {
+            $user_id = $user->userid;
+        }
+        
+        //logime inimese ka wordpressi sisse
+        IdcardAuthenticate::setSession($identityCode, $firstName, $lastName);
+        wp_set_auth_cookie($user_id);
+        if (array_key_exists('redirect_to', $_GET)) {
+            header('Location: ' . $_GET['redirect_to']);
+        } else {
+            header('Location: ' . home_url());
+        }
+    }
+
+    //küsime idid käest inimese andmeid
+    function getUserFromIdid($token) {
+        $curl = curl_init();
+        $url = "https://idid.ee/oauth2/getUser.php?secret=6868b692897ef36042c46295fe51f080&token=" . $token;
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        return $result;
+    }
+
+    //sisestame inimese andmebaasi
+    private static function createUser($userName, $firstName, $lastName, $identityCode) {
+        global $wpdb;
+        $user_data = array(
+            'user_pass' => wp_generate_password(64, true),
+            'user_login' => $userName,
+            'display_name' => "$firstName $lastName",
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'role' => get_option('default_role') // Use default role or another role, e.g. 'editor'
+        );
+        $user_id = wp_insert_user($user_data);
+        $wpdb->insert($wpdb->prefix . "idcard_users", array(
+            'firstname' => $firstName,
+            'lastname' => $lastName,
+            'identitycode' => $identityCode,
+            'userid' => $user_id,
+            'created_at' => current_time('mysql'),
+                )
+        );
+        return $user_id;
+    }
+
+    //vaatame, kas selle isikukoodiga inimene on juba baasis olemas
+    private static function getUser($identityCode) {
+        global $wpdb;
+        $user = $wpdb->get_row(
+                $wpdb->prepare(
+                        "select * from $wpdb->prefix" . "idcard_users
+		 WHERE identitycode=%s		 
+		", $identityCode
+                )
+        );
+        return $user;
+    }
+
+    //jätame kasutaja andmed sessiooni meelde
+    private static function setSession($identityCode, $firstName, $lastName) {
+        $_SESSION['identitycode'] = $identityCode;
+        $_SESSION['firstname'] = $firstName;
+        $_SESSION['lastname'] = $lastName;
+    }
+
+}
