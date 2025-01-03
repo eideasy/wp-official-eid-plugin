80582280c0bd6d72ac99110aedcc13d3957a0ae2
Mats-Joonas Kulla <matsjoonas@gmail.com>
2025-01-03 14:26:06 +0200

    Bump the version to 5.3.1 and update the changelog

--- eid-easy.php
+++ eid-easy.php
@@ -3,7 +3,7 @@
  * Plugin Name: eID Easy
  * Plugin URI: https://eideasy.com/
  * Description: Allow your visitors to login to Wordpress ID-card, Mobile-ID, Smart-ID mobile app, eParaksts, ZealID and other eID means.
- * Version: 5.3.0
+ * Version: 5.3.1
  * Author: EID Easy OÜ
  * Text Domain: eid-easy
  * Domain path /languages
